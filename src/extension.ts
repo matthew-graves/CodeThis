da6c925ca131d920fda7f5b7eeccb3d06c2b9e0e
Matthew Graves <matthew.graves@maptions.com>
2021-09-17 20:46:29 -0700

    v 0.0.2a -> multiline comments

--- src/extension.ts
+++ src/extension.ts
@@ -116,10 +116,71 @@ const singleSentenceCommandDisposable = (config: any) => {
 	});
 };
 
+const multiLineCommandDisposable = (config: any) => {
+
+	return vscode.commands.registerCommand('explain-this.explainThisMultiLine', async () => {
+
+		function formatText(inputText: string) {
+			let outputText = '"""\n1.' + inputText
+			outputText = outputText.replace(new RegExp("code above", "g"), "code below") + "\n";		
+			return outputText + '"""\n';
+
+		}
+
+		try {
+			if (config.apikey === "unset" || config.apikey === "") {
+				vscode.window.showErrorMessage("No API Key Configured, please edit settings.json File and add an API Key");
+				return false;
+			}
+			const client = new OpenAI(config.apikey);
+			// Get the active text editor
+			const editor = vscode.window.activeTextEditor;
+
+			if (editor) {
+				try {
+
+					const document = editor.document;
+					const selection = editor.selection;
+
+					const word = document.getText(selection);
+
+					(async () => {
+						const gptResponse = await client.complete({
+							engine: 'davinci-codex',
+							prompt: word + '\n\n"""\nHere\'s what the above code is doing:\n1.',
+							maxTokens: 2048,
+							temperature: 0,
+							topP: 1,
+							presencePenalty: 0,
+							frequencyPenalty: 0,
+							stop: ['"""',]
+						});
+						editor.edit(editBuilder => {
+							editBuilder.insert(selection.start, formatText(gptResponse.data.choices[0].text));
+								
+						});
+					})();
+				}
+				catch (err) {
+					console.log(err);
+				}
+			}
+		}
+		catch {
+			vscode.window.showErrorMessage("There was an issue connecting to the API, please validate your API Key");
+			return false;
+		}
+
+	});
+};
+
+
 export function activate(context: vscode.ExtensionContext) {
 	let disposable: vscode.Disposable | null = null;
 	disposable = singleLineCommandDisposable(vscode.workspace.getConfiguration('explainthis'));
 	context.subscriptions.push(disposable);
 	disposable = singleSentenceCommandDisposable(vscode.workspace.getConfiguration('explainthis'));
+	context.subscriptions.push(disposable);
+	disposable = multiLineCommandDisposable(vscode.workspace.getConfiguration('explainthis'));
 	context.subscriptions.push(disposable);
 }
